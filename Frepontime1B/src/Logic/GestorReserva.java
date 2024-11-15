5c5ce34d4b2b009f4d804618c5f63ccf051e3052
JuanMateoQ <juan.quisilema@epn.edu.ec>
2024-11-15 05:34:21 -0500

    uso de package

--- Frepontime1B/src/Logic/GestorReserva.java
+++ Frepontime1B/src/Logic/GestorReserva.java
@@ -3,22 +3,30 @@ package Logic;
 import java.util.ArrayList;
 
 public class GestorReserva {
-
     private ArrayList<Reserva> reservasDeEstudiantes;
     private ArrayList<Juego> juegos;
 
-    public GestorReserva(){
+    private static GestorReserva instance;
+
+    private GestorReserva(){
         reservasDeEstudiantes = new ArrayList();
         juegos = new ArrayList();
         //TODO: leer los atributos del txt
         //TODO: va a ser la clase principal, todos los demás gestores se inicializan aquí
 
     }
-
+    public static GestorReserva getInstance() {
+        if (instance == null) {
+            instance = new GestorReserva();
+        }
+        return instance;
+    }
+    //TODO: seguir haciendo :(
     public boolean crearReserva(Juego juego, Horario horario) {
+
         Reserva aux = new Reserva(reservasDeEstudiantes.size(), juego, horario);
         reservasDeEstudiantes.add(aux);
-
+        return true;
     }
 
     public ArrayList<Juego> getJuegosDisponibles() {
